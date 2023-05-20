8ef95090d4430e37ac488fc4c124215baf4e57d7
Veranika Kasparevych <veranikabarel@gmail.com>
2023-05-20 16:44:56 +0200

    add project page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,10 @@
 'use client';
 import { CardCustom } from '@/components/CardCustom/CardCustom.styled';
+import projects from '@/mock/projects.json';
 import { Grid, Stack } from '@mui/material';
 
-const projects = [
-  {
-    img: '/images/wool.png',
-    alt: 'alt1',
-    title: 'Project 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    action: 'Learn more',
-  },
-  {
-    img: '/images/wool.png',
-    alt: 'alt2',
-    title: 'Project 2',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    action: 'Learn more',
-  },
-  {
-    img: '/images/wool.png',
-    alt: 'alt3',
-    title: 'Project 3',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    action: 'Learn more',
-  },
-];
+export default function Dashboard() {
 
-export default function Home() {
   return (
     <Grid
       container
@@ -46,15 +21,10 @@ export default function Home() {
         p={2}
         borderRadius={1}
       >
-        {projects.map(project => (
-          <CardCustom
-            key={project.title}
-            img={project.img}
-            alt={project.alt}
-            title={project.title}
-            description={project.description}
-            action={project.action}
-          />
+        {projects.projects.map(project => (
+          <CardCustom key={project.title} {...project} >
+
+          </CardCustom>
         ))}
       </Stack>
     </Grid>
