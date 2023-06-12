c58e3671a832fff59de122f3cf311e80187c6fde
Veranika Kasparevych <veranikabarel@gmail.com>
2023-06-12 14:54:15 +0200

    update routes

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { Grid } from '@mui/material';
+
+export default function Login() {
+  return (
+    <Grid
+      container
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      direction="column"
+    >
+      <h1>Login page</h1>
+    </Grid>
+  );
+}
---
+++ app/overwiev/page.tsx
@@ -0,0 +1,3 @@
+export default function Overwiev() {
+  return <div>Overwiev</div>;
+}
