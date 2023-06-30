6cdf6e62c4ea52386abd3893bcd59b917b71803d
Veranika Kasparevych <veranikabarel@gmail.com>
2023-06-30 16:21:20 +0200

    refactor

--- app/patterns/page.tsx
+++ app/patterns/page.tsx
@@ -1,43 +1,25 @@
-import SearchBar from '@//modules/SearchBar/SearchBar';
-import Categories from '@/modules/Categories/Categories';
-import PatternGallery from '@/modules/PatternGallery/PatternGallery';
+'use client';
 
 const url = process.env.RAVELRY_BASE_URL;
 const username = process.env.RAVELRY_USERNAME;
 const password = process.env.RAVELRY_PASSWORD;
 
-async function getPatterns() {
-  const res = await fetch(`${url}/patterns.json?ids=600+601`, {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Basic ${btoa(username + ':' + password)}`,
-    },
-  });
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-  return res.json();
-}
+// async function getPatterns() {
+//   const res = await fetch(`${url}/patterns.json?ids=600+601`, {
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Authorization: `Basic ${btoa(username + ':' + password)}`,
+//     },
+//   });
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data');
+//   }
+//   return res.json();
+// }
 
 export default async function PatternLibrary() {
-  const data = await getPatterns();
-  console.log(data);
+  // const data = await getPatterns();
+  // console.log(data);
 
-  return (
-    <div>
-      <div>
-        {/* @ts-expect-error Server Component */}
-        <SearchBar />
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <div>
-          {/* @ts-expect-error Server Component */}
-          <Categories />
-        </div>
-        <div>
-          <PatternGallery />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>PatternLibrary</div>;
 }
